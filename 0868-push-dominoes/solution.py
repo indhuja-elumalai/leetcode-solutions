9b385f07e9738222e7fcdaff1adb832a447d333d
indhuja-elumalai <indhujabusiness@gmail.com>
2026-01-28 10:15:27 +0530

    clean-up

--- 0868-push-dominoes/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def pushDominoes(self, dominoes: str) -> str:
-            prev=""
-            while dominoes!=prev:
-                prev=dominoes
-                dominoes=dominoes.replace('R.L','xxx').replace('.L','LL').replace('R.','RR')
-            return dominoes.replace('xxx','R.L')
-
-
